9cdaf5b9ed4329c588333ae749d4a6f45ef9df97
malicemazerrr <rwizeyeon1@gmail.com>
2026-04-21 14:54:58 +0700

    Add files via upload

---
+++ LatihanMemberClass/LatihanMemberClass/PlaneTicket.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatihanMemberClass
+{
+    internal class PlaneTicket
+    {
+        private string _origin;
+        private string _destination;
+        private float _cost;
+        private string _currency;
+
+        public string Origin
+        {
+            get { return _origin; }
+            set { _origin = value; }
+        }
+        public string Destination
+        {
+            get { return _destination; }
+            set { _destination = value; }
+        }
+        public float Cost
+        {
+            get { return _cost; }
+            set { _cost = value; }
+        }
+        public string Currency
+        {
+            get { return _currency; }
+            set { _currency = value; }
+        }
+
+        public void PrintSummary()
+        {
+            Console.WriteLine("");
+        }
+    }
+}
